ac0bf58933b8c7c239216806692a9f6ae5ee1f5c
Dusan Petkovic <dulesaga@gmail.com>
2023-11-18 12:57:49 +0100

    update firebase config location

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,10 +1,11 @@
 import admin, { ServiceAccount } from "firebase-admin";
-import serviceAccount from './env__serviceAccountKey.json'
+import serviceAccount from '@/../env__serviceAccountKey.json'
 
 if (!admin.apps.length) {
   try {
+    const firebaseConfig = process.env.FIREBASE_CONFIG ? JSON.parse(process.env.FIREBASE_CONFIG) : serviceAccount; 
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as ServiceAccount)
+      credential: admin.credential.cert(firebaseConfig as ServiceAccount)
     });
   } catch (error) {
     if(error instanceof Error)
