b5185381c4df71647b3b5bfc812b6eabae321efe
Dusan Petkovic <dulesaga@gmail.com>
2023-11-18 13:36:13 +0100

    update firebase config to string

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,22 +2,13 @@ import admin, { ServiceAccount } from "firebase-admin";
 
 if (!admin.apps.length) {
   try {
-    const firebaseConfig = {
-      type: "service_account",
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY,
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      client_id: process.env.FIREBASE_CLIENT_ID,
-      auth_uri: process.env.FIREBASE_AUTH_URI,
-      token_uri: process.env.FIREBASE_TOKEN_URI,
-      auth_provider_x509_cert_url:
-        process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-      client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-      universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-    };
+  
+    const serviceAccount = JSON.parse(
+      process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+    );
+    
     admin.initializeApp({
-      credential: admin.credential.cert(firebaseConfig as ServiceAccount),
+      credential: admin.credential.cert(serviceAccount as ServiceAccount),
     });
   } catch (error) {
     if (error instanceof Error)
