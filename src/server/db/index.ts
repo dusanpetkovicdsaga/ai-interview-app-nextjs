765fd3007c917108a904ccd6c14e603409aaaa52
Dusan Petkovic <dulesaga@gmail.com>
2023-11-18 13:11:04 +0100

    update firebase config to env vars

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,15 +1,27 @@
 import admin, { ServiceAccount } from "firebase-admin";
-import serviceAccount from '@/../env__serviceAccountKey.json'
 
 if (!admin.apps.length) {
   try {
-    const firebaseConfig = process.env.FIREBASE_CONFIG ? JSON.parse(process.env.FIREBASE_CONFIG) : serviceAccount; 
+    const firebaseConfig = {
+      type: "service_account",
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_AUTH_URI,
+      token_uri: process.env.FIREBASE_TOKEN_URI,
+      auth_provider_x509_cert_url:
+        process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+      universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+    };
     admin.initializeApp({
-      credential: admin.credential.cert(firebaseConfig as ServiceAccount)
+      credential: admin.credential.cert(firebaseConfig as ServiceAccount),
     });
   } catch (error) {
-    if(error instanceof Error)
-    console.log('Firebase admin initialization error', error.stack);
+    if (error instanceof Error)
+      console.log("Firebase admin initialization error", error.stack);
   }
 }
 export default admin.firestore();
