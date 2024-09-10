54fb19218e6d29cb1de370c3d873c680cabd5c9f
dusanpetkovicdsaga <dukisaga@yahoo.com>
2024-09-10 13:31:34 +0200

    fix issues

--- src/features/StarterPage/StarterPage.tsx
+++ src/features/StarterPage/StarterPage.tsx
@@ -12,74 +12,66 @@ import useInterviewStore, {
   TStoreActions,
 } from "@/store/useInterviewStore";
 import { useRouter } from "next/navigation";
-import { SCHEMA, TExperienceLevelKeys, TInterviewRoleKeys } from "@/shared";
+import { SCHEMA } from "@/shared";
 import Loader from "@/components/Loader";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { useGenerateQuestionsMutation } from "@/services/generateQuestions";
-import {
-  interviewRoles,
-  experienceLevels,
-  interviewQuestionsCount,
-} from "@/constants";
+
 import { PageContentBox } from "@/layout/PageContentBox";
-import Icon from "@/assets/ai.png";
-import Image from "next/image";
-import ReCAPTCHA from "react-google-recaptcha";
 import React from "react";
 import { Step1 } from "./components/Step1";
 import { Step2 } from "./components/Step2";
 import { Step3 } from "./components/Step3";
 
-
-const MemoizedReCAPTCHA = React.memo(ReCAPTCHA);
-
 export function StarterPage() {
   const { push: navigate } = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
-  const [currentStep, setCurrentStep] = useState(1)
+  const [currentStep, setCurrentStep] = useState(1);
 
   const renderCurrentStep = () => {
     if (currentStep === 1) {
-      return <Step1 onSubmit={() => { setCurrentStep(2) }}></Step1>
+      return (
+        <Step1
+          onSubmit={() => {
+            setCurrentStep(2);
+          }}
+        ></Step1>
+      );
     } else if (currentStep === 2) {
-      return <Step2 onSubmit={() => { setCurrentStep(3) }}></Step2>
+      return (
+        <Step2
+          onSubmit={() => {
+            setCurrentStep(3);
+          }}
+        ></Step2>
+      );
     } else if (currentStep === 3) {
-      return <Step3 onSubmit={() => { handleGenerateQuestions() }}></Step3>
+      return (
+        <Step3
+          onSubmit={() => {
+            handleGenerateQuestions();
+          }}
+        ></Step3>
+      );
     }
-  }
+  };
 
   const selectConfig = (state: TStore) => state.config;
-  const selectSetConfig = (state: TStoreActions) => state.setConfig;
-  const selectUser = (state: TStoreActions) => state.setUser;
   const selectUserData = (state: TStore) => state.user;
   const selectQuestions = (state: TStoreActions) => state.setQuestions;
   const selectErrors = (state: TStoreActions) => state.setErrors;
 
-  const setConfig = useInterviewStore(selectSetConfig);
-  const setUser = useInterviewStore(selectUser);
   const setQuestions = useInterviewStore(selectQuestions);
 
   const config = useInterviewStore(selectConfig);
   const user = useInterviewStore(selectUserData);
 
   const recaptchaToken = user.recaptchaToken;
-  const setRecaptchaToken = (token: string) => {
-    setUser({ recaptchaToken: token });
-  };
 
   const setErrors = useInterviewStore(selectErrors);
 
   const { mutateAsync: generateQuestions } = useGenerateQuestionsMutation();
-
-  const handleRecaptchaChange = useCallback(
-    (value: string | null) => {
-      if (!value) return;
-
-      setRecaptchaToken(value);
-    },
-    [setRecaptchaToken]
-  );
 
   const handleGenerateQuestions = async () => {
     try {
@@ -106,7 +98,7 @@ export function StarterPage() {
       // transition to the next page (INREVIEW PAGE)
       navigate("/qa");
     } catch (err) {
-      console.log(err);
+      console.error(err);
       setErrors([
         {
           message: "Something went wrong. Please try again later.",
@@ -119,18 +111,10 @@ export function StarterPage() {
   };
 
   return (
-
     <Loader isLoading={isLoading}>
       <PageContentBox className=" border-blue-400 border-4 h-screen">
-
-        <div>
-          {renderCurrentStep()}
-        </div>
-
+        <div>{renderCurrentStep()}</div>
       </PageContentBox>
     </Loader>
-
-
-
   );
 }
--- src/server/api/apiSendSuccessMail.ts
+++ src/server/api/apiSendSuccessMail.ts
@@ -1,25 +1,21 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { EmailTemplate } from '@/email-templates/success';
-import { Resend } from 'resend';
-import { User } from '@/store/useInterviewStore';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { EmailTemplate } from "@/email-templates/success";
+import { Resend } from "resend";
+import { User } from "@/store/useInterviewStore";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function apiSendSuccessMail(resultLink: string, user: User) {
-
   if (!user.email) {
-    return false
+    return false;
   }
 
-
-
   const { data, error } = await resend.emails.send({
-    from: 'Ai Bot <mail@ai-bot.dsaga.dev>',
+    from: "Ai Bot <mail@email.dsaga.dev>",
     to: [user.email],
-    subject: 'AI Bot - Your interview results',
+    subject: "AI Bot - Your interview results",
     react: EmailTemplate({ resultLink }),
   });
   console.log("sending success email", data, error);
-  return true
-
+  return true;
 }
